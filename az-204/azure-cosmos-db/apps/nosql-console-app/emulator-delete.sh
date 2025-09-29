93cd51af6523480de78b8367df9256d066be0b70
josephaw1022 <josephsims1@gmail.com>
2025-09-29 01:53:50 -0400

    add volumes

--- az-204/azure-cosmos-db/apps/nosql-console-app/emulator-delete.sh
+++ az-204/azure-cosmos-db/apps/nosql-console-app/emulator-delete.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-podman rm -f linux-emulator
+podman rm -f linux-emulator || true
+podman volume rm cosmosdb-data || true
+
 echo "Deleted container: linux-emulator"
--- az-204/azure-cosmos-db/apps/nosql-console-app/emulator-latest.sh
+++ az-204/azure-cosmos-db/apps/nosql-console-app/emulator-latest.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-
 podman run --replace --name linux-emulator -d --restart=unless-stopped \
   -e AZURE_COSMOS_EMULATOR_PARTITION_COUNT=250 \
   -e AZURE_COSMOS_EMULATOR_IP_ADDRESS_OVERRIDE=127.0.0.1 \
+  -e AZURE_COSMOS_EMULATOR_ENABLE_DATA_PERSISTENCE=true \
+  -v cosmosdb-data:/tmp/cosmos/appdata \
   -p 8081:8081 \
   -p 10250-10255:10250-10255 \
   mcr.microsoft.com/cosmosdb/linux/azure-cosmos-emulator:latest
